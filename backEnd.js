321f843e535b099cb75593da0156223ac697b67f
vyckPFH <victoria.c.oliveira08@gmail.com>
2026-06-02 23:29:14 -0300

    If i were a zombie, i'm never eat your brain

--- backEnd.js
+++ backEnd.js
@@ -9,13 +9,13 @@ function limpar() {
 }
 
 function apagar() {
-    display.value = display.value.slice(0, -1);
+    display.value = display.value.slice(0, -1); //slice() -> recorta a string, neste caso, remove o último caractere.
 }
 
 function calcular() {
 
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value); //eval() -> interpreta string como expressão matemática.
     }
     catch {
         display.value = "Erro";
@@ -26,7 +26,7 @@ function calcular() {
 function porcentagem() {
 
     if (display.value !== "") {
-        display.value = Number(display.value) / 100;
+        display.value = Number(display.value) / 100; //Number() -> converte a string p número.
     }
 
 }
@@ -34,7 +34,7 @@ function porcentagem() {
 function raiz() {
 
     if (display.value !== "") {
-        display.value = Math.sqrt(Number(display.value));
+        display.value = Math.sqrt(Number(display.value)); //Math.sqrt() -> calcula a raiz quadrada de um número.
     }
 
 }
@@ -43,10 +43,11 @@ function logaritmo() {
 
     if (display.value !== "") {
 
-        const numero = Number(display.value);
+        const numero = Number(display.value); // converte string q user digita p numero.
 
         if (numero > 0) {
             display.value = Math.log10(numero);
+            
         }
         else {
             display.value = "Erro";
